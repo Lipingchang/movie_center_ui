fefabf5e84119bfbdfa4af85465219bfb788a880
lipc_1700 <3236568459@somechatapp.com>
2021-10-15 22:37:40 +0800

    升级electron后需要修改配置，才能用remote模块

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ function createWindow () {
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
+      enableRemoteModule: true,
       // webSecurity: false,
     }
   })
